6bbeb20c32ae25e9ee7e815e114c49d2fc18b8bb
Wahid Ali <1213089@dtu.edu.eg>
2025-03-05 08:40:55 +0200

    Introduce Splash Screen and its utils

---
+++ app/src/main/java/org/wahid/noted/feature_note/presentation/note_screen/component/DefaultRadioButton.kt
@@ -0,0 +1,10 @@
+package org.wahid.noted.feature_note.presentation.note_screen.component
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun DefaultRadioButton(modifier: Modifier = Modifier) {
+    
+}
